f3c611db13fbff19b019c683a98b71ecfdf49cd6
mlyx <mlyxdev@gmail.com>
2020-08-28 14:09:02 +0800

    init

---
+++ iPhoneOS13.6.1.sdk/System/Library/PrivateFrameworks/install.sh
@@ -0,0 +1,3 @@
+# sudo mkdir /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/PrivateFrameworks/
+#!/bin/bash
+sudo cp -r   /Users/dominic/Desktop/PrivateFrameworks /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/
